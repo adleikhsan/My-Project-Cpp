813a7190c25a9eceba4e21c494e98489bd965527
Adile Ikhsan Muhammad Al Masyhuri <adileikhsan1902@gmail.com>
2024-10-11 20:58:30 +0700

    Add files via upload

---
+++ CalculatorSederhana.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	float a,b,hasil;
+	char Aritmatika;
+
+	//title
+	cout << "Selamat Datang Di Program Calculator Sederhana\n\n";
+
+	cout << "Masukkan angka pertama: ";
+	cin >> a;
+	cout << "Masukkan Operasi +,-,/,* : ";
+	cin >> Aritmatika;
+	cout << "Masukkan angka kedua: ";
+	cin >> b;
+
+	if (Aritmatika == '+') {
+		hasil = a + b;
+	}
+	else if (Aritmatika == '-') {
+		hasil = a - b;
+	}
+	else if (Aritmatika == '/') {
+		hasil = a / b;
+	}
+	else if (Aritmatika == '*') {
+		hasil = a * b;
+	}
+	else {
+		cout << "Operasi anda tidak sesuai!";
+	}
+		cout << "hasil dari perhitungan adalah: " << hasil << endl;
+
+
+		cin.get();
+		return 0;
+}
